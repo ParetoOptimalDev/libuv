d02cc4bd98f4b3bce4c6afe0ad7f39c1aaaaa800
Ben Noordhuis <info@bnoordhuis.nl>
2022-09-01 09:35:33 +0200

    test: fix musl libc.a dlerror() test expectation
    Dynamic loading with musl only works with libc.so because it is
    is implemented in musl's dynamic linker, not in libc proper.
    
    libc.a contains just a stub dlopen() that always fails with a "Dynamic
    loading not supported" error message. Update the test to handle that.
    
    Fix: https://github.com/libuv/libuv/issues/3706

--- test/test-dlerror.c
+++ test/test-dlerror.c
@@ -50,7 +50,11 @@ TEST_IMPL(dlerror) {
   /* Should return the same error twice in a row. */
   msg = uv_dlerror(&lib);
   ASSERT_NOT_NULL(msg);
-#if !defined(__OpenBSD__) && !defined(__QNX__)
+#if defined(__linux__)
+  /* musl's libc.a does not support dlopen(), only libc.so does. */
+  if (NULL == strstr(msg, "Dynamic loading not supported"))
+    ASSERT_NOT_NULL(strstr(msg, path));
+#elif !defined(__OpenBSD__) && !defined(__QNX__)
   ASSERT_NOT_NULL(strstr(msg, path));
 #endif
   ASSERT_NULL(strstr(msg, dlerror_no_error));
